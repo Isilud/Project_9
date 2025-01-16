b2a0603065d177e06ef30daee22ea3a6b32ace58
Isilud <jolan.houot@gmail.com>
2025-01-16 15:48:03 +0100

    frontservice: add login page for future implementation and router

--- frontservice/src/app/layout/Main.tsx
+++ frontservice/src/app/layout/Main.tsx
@@ -1,7 +1,26 @@
 import { JSX } from "react";
 import "./Main.scss";
 import PatientPage from "../page/PatientPage";
+import { Route, Routes } from "react-router-dom";
+import { userLoginState } from "../store/loginAtom";
+import { useAtom } from "jotai";
+import LoginPage from "../page/LoginPage";
 
 export default function Main(): JSX.Element {
-  return <PatientPage />;
+  const [userLogin] = useAtom(userLoginState);
+
+  return (
+    <Routes>
+      <Route path="/">
+        {userLogin.logged ? (
+          <>
+            <Route index element={<PatientPage />} />
+            <Route path="addPatient" element={<></>} />
+          </>
+        ) : (
+          <Route index element={<LoginPage />} />
+        )}
+      </Route>
+    </Routes>
+  );
 }
